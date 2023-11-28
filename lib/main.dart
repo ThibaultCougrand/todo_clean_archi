f9e5a7a5203f008a29abd920ed84d2212349590a
thibaultc <thibault.cougrand@gmail.com>
2023-11-28 22:20:26 +0100

    Mise en place de sembast

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todo_clean_archi/src/app.dart';
+import 'package:todo_clean_archi/src/infrastructure/local_db/i_local_db.dart';
+import 'package:todo_clean_archi/src/infrastructure/local_db/sembast/my_sembast.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  ///* Initialisation des dépendances (infrastructure)
+  final ILocalDb localDb = await MySembast.initDb();
+
+  ///* Implémente les provider initialisés au lancement
+  final container = ProviderContainer(
+    overrides: [
+      localDbProvider.overrideWithValue(localDb),
+    ],
+  );
+
+  ///* Initialise la lecture des providers
+  container.read(localDbProvider);
+
+  runApp(UncontrolledProviderScope(
+    container: container,
+    child: const MyApp(),
+  ));
 }
